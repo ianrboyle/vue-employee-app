7b2e65a10ad65357910e0f6c7dc9a2020539a263
Ian Boyle <irboyle21@gmail.com>
2021-11-30 13:38:05 -0500

    adds edit, delete, new - basically all crud methods

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app" class="small-container">
     <h1>Employees</h1>
-
-    <employee-table :employees="employees" />
-    <employee-form :employee="employee" />
+    <employee-form @add:employee="addEmployee" />
+    <employee-table :employees="employees" @delete:employee="deleteEmployee" @edit:employee="editEmployee" />
   </div>
 </template>
 
@@ -37,6 +36,21 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    addEmployee(employee) {
+      const lastId = this.employees.length > 0 ? this.employees[this.employees.length - 1].id : 0;
+      const id = lastId + 1;
+      const newEmployee = { ...employee, id };
+
+      this.employees = [...this.employees, newEmployee];
+    },
+    deleteEmployee(id) {
+      this.employees = this.employees.filter((employee) => employee.id !== id);
+    },
+    editEmployee(id, updatedEmployee) {
+      this.employees = this.employees.map((employee) => (employee.id === id ? updatedEmployee : employee));
+    },
   },
 };
 </script>
